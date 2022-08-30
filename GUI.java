612b9e5bf03a989f669e3ae062de5968c6037200
Jules f4iey <93203507+f4iey@users.noreply.github.com>
2022-08-30 18:21:31 +0200

    add basic GUI components

--- GUI.java
+++ GUI.java
@@ -1,1 +1,11 @@
-
+import javax.swing.*
+public class GUI extends JFrame{
+  public GUI(){
+  //set look and feel by defaut on GTK+
+  UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+    super("Oselog Contest Logger");
+    setSize(1280, 720);
+    setVisible(true);
+  
+  
+}
