edec44b03dbdc2a879d921c3ba912f6f6df7719e
Jules f4iey <93203507+f4iey@users.noreply.github.com>
2022-08-30 15:41:17 +0200

    initial commit

---
+++ Log.java
@@ -0,0 +1,14 @@
+public class Log{
+ //ATTs
+ private String callsign;
+ private String name;
+  //...blablabla
+ public Log(){
+  callsign = "N0CALL";
+ }
+  
+//Need JetBrains to create auto getters and setters...  
+  
+  
+  
+}
